c4691c08a8b09444e7cabd4476bd05a8f18872c9
dimv <vottasdim@gmail.com>
2022-09-13 19:19:19 +0300

    init

---
+++ src/test/java/com/example/techassessmentaccepted/TechAssessmentAcceptedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techassessmentaccepted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAssessmentAcceptedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
